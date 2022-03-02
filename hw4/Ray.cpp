cab92ad52852342c7b6d9ad51ff04ed19c0cad2f
YufeiWeng <yufei.weng888@gmail.com>
2022-03-01 18:34:45 -0800

    working on sphere

--- hw4/Ray.cpp
+++ hw4/Ray.cpp
@@ -1,11 +1,10 @@
 #include "Ray.h"
 
-
 glm::vec3 ori;
 glm::vec3 dir;
 
 
-Ray(const glm::vec3& origin, const glm::vec3& direction) {
+Ray(const glm::vec3 &origin, const glm::vec3 &direction) {
 	ori = origin;
 	dir = direction;
 }
--- hw4/main.cpp
+++ hw4/main.cpp
@@ -1,33 +1,63 @@
 #include "main.h"
+#include "Ray.h"
 #include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>  //If there are redlines, go and check: https://www.google.com/search?q=how+to+include+glm+in+visual+studio+2019&ei=tbkeYuH6BZrHkPIPgMex2A0&ved=0ahUKEwihqa6plab2AhWaI0QIHYBjDNsQ4dUDCA4&uact=5&oq=how+to+include+glm+in+visual+studio+2019&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKsCOgcIABBHELADOgYIABAWEB46BQgAEIYDOgUIIRCgAUoECEEYAEoECEYYAFCyBVj-C2D3DmgBcAF4AYAB9AGIAYcGkgEFMC40LjGYAQCgAQHIAQjAAQE&sclient=gws-wiz#kpvalbx=_u7keYvz9FrefkPIPt8SO8Aw20
 using namespace std;
 
+
+glm::vec3 hit_sphere(const glm::vec3& center, double radius, const Ray& ray ) {
+    //at^2+bt+c=0
+    //d=sqrt(b^2-4ac)
+    //t=(-b+-d)/(2a)
+    auto a = glm::dot(ray.dir, ray.dir);
+    auto b = 2 * glm::dot(rat.dir, ray.ori - center);
+    auto c = glm::dot(ray.ori - center, ray.ori - center) - radius * radius;
+    auto d = b * b - 4 * a * c;
+    //two intersections
+    if (d > 0) {
+        //red for now
+        return glm::vec3(1.0, 0.0, 0.0);
+    }
+    //one or none
+    else {
+        //background is black
+        return glm::vec3(0.0, 0.0, 0.0);
+    }
+}
+
+
 int main() {
 
     // Image
 
-    const int image_width = 256;
-    const int image_height = 256;
+    const int image_width = 640; //from test scene 
+    const int image_height = 480;
 
     // Camera
+    glm::vec3 eye(0.0, 0.0, 0.0); //0 for now 
+    glm::vec3 center(0.0, 0.0, 0.0);
+    float fovy(0);
+    glm::vec3 up(0.0, 0.0, 0.0);
+
+
+    //uwv
+    glm::vec3 u, w, v;
+    w = glm::normalize(eye);
+    u = glm::normalize(glm::cross(up, w));
+    v = glm::cross(w, u);
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    //std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j) {
-        for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+    for (int i = 0; i < image_height; ++i) {
+        for (int j = 0; j < image_width; ++j) {
+            float alpha = tan(fovy / 2) * ((j - image_width / 2) / (image_width / 2));
+            float beta = tan(fovy / 2) * ((image_height / 2 - i) / (image_height / 2));
+            glm::vec3 direction = normalize(alpha * u + beta * v - w);
+            Ray r(eye, direction);
         }
     }
 }
+
