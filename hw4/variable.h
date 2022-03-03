90cc70ad7c2975be9f2aa8bc103f13f6e0c9d709
YufeiWeng <yufei.weng888@gmail.com>
2022-03-02 21:22:33 -0800

    change const to extern

--- hw4/variable.h
+++ hw4/variable.h
@@ -1,5 +1,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <vector>
 typedef glm::vec3 vec3;
 
 #ifdef MAINPROGRAM 
@@ -8,15 +9,16 @@ typedef glm::vec3 vec3;
 #define EXTERN extern 
 #endif
 
-const int image_width = 640; //from test scene 
-const int image_height = 480;
-const float fovy(45);
-const float fovx = fovy * (image_width / (image_height *1.0));
-const vec3 eye(-4, -4, 4); //0 for now 
-const vec3 center(0.0, 1.0, 0.0);
+extern int image_width = 640; //from test scene 
+extern int image_height = 480;
+extern float fovy(45);
+extern float fovx = fovy * (image_width / (image_height *1.0));
+extern vec3 eye(-4, -4, 4); //0 for now 
+extern vec3 center(0.0, 1.0, 0.0);
 
-const vec3 up(1.0, 0.0, 0.0);
-const vec3 A(2.0, 4.0, 1.0);
-const vec3 B(0.0, 0.0, 1.0);
-const vec3 C(0.0, -1.0, 3.0);  
+extern vec3 up(1.0, 0.0, 0.0);
+extern vec3 A(2.0, 4.0, 1.0);
+extern vec3 B(0.0, 0.0, 1.0);
+extern vec3 C(0.0, -1.0, 3.0);
+
 
