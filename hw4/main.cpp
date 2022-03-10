a243968c55d444ac92142d4177f9c18f2f7e8b85
YufeiWeng <yufei.weng888@gmail.com>
2022-03-09 21:37:18 -0800

    prepare for merging

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -215,13 +215,13 @@ float computeV(vec3& intP, vec4& lightDir) {
     Ray ray;
     //if directional
     if (lightDir[3] == 0) {
-        ray.dir = normalize(-vec3(lightDir[0], lightDir[1], lightDir[2]));
-        ray.ori = intP+ (float(0.01) * ray.dir);
+        ray.dir = -normalize(vec3(lightDir[0], lightDir[1], lightDir[2]));
+        ray.ori = intP+ (float(0.001) * ray.dir);
     }
     //point
     else {
         ray.dir = normalize(vec3(lightDir) - intP);
-        ray.ori = intP + (float(0.01) * ray.dir);
+        ray.ori = intP + (float(0.001) * ray.dir);
     }
     hit_record h = Intersection(obj, ray);
     if (h.t == -1) {
